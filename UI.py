bc1971301c58dc1ad2a8c14992e0e79414c83dd2
Alex-pv-3-50 <150586458+Alex-pv-3-50@users.noreply.github.com>
2023-11-12 13:16:13 +0300

    Create UI.py

---
+++ UI.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Третья программа')
+
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(100, 100)
+        self.btn.move(100, 100)
+        # присоединим к событию нажатия на кнопку обработчик self.hello()
+        self.btn.clicked.connect(self.hello)
+
+    def hello(self):
+        # метод setText() используется для задания надписи на кпопке
+        self.btn.setText('Привет')
